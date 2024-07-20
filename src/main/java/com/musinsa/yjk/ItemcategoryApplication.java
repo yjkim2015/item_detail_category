1a4591aebca59f895305d1878f6a0a4cffb85eff
김윤제 <forteas2003@hanmail.net>
2024-07-20 14:49:36 +0900

    first commit

---
+++ src/main/java/com/musinsa/yjk/ItemcategoryApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.yjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemcategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemcategoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musinsa/yjk/ItemcategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.yjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemcategoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
